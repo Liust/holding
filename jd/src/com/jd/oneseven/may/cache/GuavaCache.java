15e898833b99824d700b265208014bb9746813a8
liushitao <liushitao@jd.com>
2017-05-18 15:24:39 +0800

    beginning

--- jd/src/com/jd/oneseven/may/cache/GuavaCache.java
+++ jd/src/com/jd/oneseven/may/cache/GuavaCache.java
@@ -1,6 +1,9 @@
 package com.jd.oneseven.may.cache;
 
 import com.google.common.cache.Cache;
+import com.google.common.cache.CacheBuilder;
+
+import java.util.concurrent.TimeUnit;
 
 /**
  * Created by liushitao on 2017/5/18.
@@ -8,27 +11,45 @@ import com.google.common.cache.Cache;
 public class GuavaCache<T> {
     Cache<String, T> cache;
 
+    void initCache(){
+       if(null == cache){
+           CacheBuilder cacheBuilder = CacheBuilder.newBuilder();
+           int concurrencyLevel = 1;
+           int initialCapacity = 10;
+           long maximumSize = 10;
+           long expire = 120;
+           cacheBuilder.concurrencyLevel(concurrencyLevel);
+           cacheBuilder.initialCapacity(initialCapacity);
+           cacheBuilder.maximumSize(maximumSize);
+
+           cacheBuilder.expireAfterAccess(expire, TimeUnit.SECONDS);
+//           cacheBuilder.expireAfterWrite();
+
+           cache = cacheBuilder.build();
+       }
+    }
+
     public T get(String key){
-        //init
+        initCache();
 
         return cache.getIfPresent(key);
     }
 
     public void put(String key, T obj){
-        //init
+        initCache();
 
         cache.put(key, obj);
     }
 
     public void clear(String key){
-        //init
+        initCache();
 
         cache.invalidate(key);
     }
 
 
     /* getter and setter **/
-    
+
     public Cache<String, T> getCache() {
         return cache;
     }
