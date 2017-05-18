660ec930d18124c3dcf5efd50d8f8cea8e303ca1
liushitao <liushitao@jd.com>
2017-05-18 14:31:55 +0800

    beginning

---
+++ jd/src/com/jd/oneseven/may/HelloWorld.java
@@ -0,0 +1,11 @@
+package com.jd.oneseven.may;
+
+/**
+ * Created by liushitao on 2017/5/18.
+ */
+public class HelloWorld<T> {
+    T obj;
+    public static void main(String[] arg){
+        System.out.println(" H.W.");
+    }
+}
